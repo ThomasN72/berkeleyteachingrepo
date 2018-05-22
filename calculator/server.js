58a46ac164bde089d766fb76233e22da6e86d111
Game <nghoming72@gmail.com>
2018-05-21 21:34:53 -0700

    add exercises

--- calculator/server.js
+++ calculator/server.js
@@ -1,2 +1,27 @@
 let express = require("express")
 
+let PORT = process.env.PORT || 9000;
+
+let app = express();
+
+app.get("/:operation/:firstNum/:secondNum", function(req, res){
+    let operation = req.params.operation;
+    let firsNum = parseInt(req.params.firstNum);
+    let secondNum = parseInt(req.params.secondNum);
+    let result;
+
+    switch (operation){
+        case "add":
+        case "+":
+            result = firstNum + secondNum;
+            break;
+        
+    }
+})
+
+
+
+
+app.listen(PORT, function(){
+    console.log(`Server listening on : http://localhost: ${PORT}`)
+})
